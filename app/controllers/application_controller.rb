8a3050139e7ebebe2c40d44d79d18a7173817ec7
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-06-06 15:35:23 -0400

    added current user, logged in, and authorize methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,32 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV['SESSION_PASSWORD']
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+
+      def current_user
+          User.find_by(id: session[:user_id])
+      end
+
+      def logged_in?
+          !!current_user
+      end
+
+      def authenticate
+          redirect '/login' if !logged_in?
+      end
+
+      def authorize(watchlist)
+          authenticate
+          redirect '/watchlists' if watchlist.user != current_user
+      end
+
+  end
 end
