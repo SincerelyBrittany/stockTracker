df7e4734b3cbaf95d8d56afd8cd31bd48d08d052
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-06-06 16:41:52 -0400

    added api manager

---
+++ app/helpers/api_manager.rb
@@ -0,0 +1,17 @@
+require './config/environment'
+
+class APImanager
+
+
+#
+#  def self.get_quote
+#    client = IEX::Api::Client.new(
+#      publishable_token: 'pk_2659fdc7507545b2b9cc8904c884f050',
+#      secret_token: 'sk_82baa0533c9548ad8aa46b190e32251a',
+#      endpoint: 'https://cloud.iexapis.com/v1'
+# )
+#
+# binding.pry
+#
+#  end
+end
