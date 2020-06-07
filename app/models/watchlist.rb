9149beaaa8c9de6b5b705a8da3a93f4000cad4d7
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-06-07 19:44:41 -0400

    belongs_to :user   has_many :watchlist_stocks   has_many :stocks, through: :watchlist_stocks

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,4 +1,4 @@
-class Watchlist < ActiveRecord::Base
+class Watchlist < ActiveRecord::Base #landmark
   belongs_to :user
   has_many :watchlist_stocks
   has_many :stocks, through: :watchlist_stocks
