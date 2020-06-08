dbfc737845bb100626b0196aac3170f181738a1f
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-06-08 12:09:04 -0400

    creates Stock and watchlist based of users commands

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,24 +1,20 @@
 class WatchlistsController < ApplicationController
 
-  get '/watchlists' do #works
+  get '/watchlists' do
      authenticate
      @watchlists = current_user.watchlists
      erb :'watchlists/index'
    end
 
-   get '/watchlists/new' do #works
+   get '/watchlists/new' do
      authenticate
      erb :'/watchlists/new'
    end
 
    post '/watchlists' do
      authenticate
-     # binding.pry
-      #params[:stock][:watchlist_ids]
     if params[:watchlist][:stock] == nil
      @watchlist = current_user.watchlists.build(name: params[:watchlist][:name], user_id: params[:user_id])
-#      @watchlist
-# => #<Watchlist:0x00007fefc59d1b30 id: nil, name: "b3", user_id: 2, created_at: nil, updated_at: nil>
       if @watchlist.save
         flash[:message] = "You have successfully created an order."
       redirect "/watchlists"
@@ -26,9 +22,12 @@ class WatchlistsController < ApplicationController
         flash[:error] = @watchlist.errors.full_messages
         redirect "/watchlists/new"
       end
-     # Watchlist.create(name: params[:watchlist][:name], user_id: current_user.id)
    else
-     binding.pry
+     @stock = Stock.create(name: params[:watchlist][:stock][:name], price: params[:watchlist][:stock][:price],
+     logo: params[:watchlist][:stock][:logo], ceo: params[:watchlist][:stock][:ceo],
+     description: params[:watchlist][:stock][:description], ticker: params[:watchlist][:stock][:ticker])
+     @watchlist = Watchlist.find(params[:watchlist][:id])
+     @watchlist.stocks << @stock
      redirect '/watchlists'
    end
  end
@@ -36,7 +35,6 @@ class WatchlistsController < ApplicationController
    get '/watchlists/:id/edit' do
      @watchlist = Watchlist.find_by(id: params[:id])
      authorize(@watchlist)
-     # binding.pry
      erb :'/watchlists/edit'
    end
 
@@ -52,12 +50,10 @@ class WatchlistsController < ApplicationController
 
    get '/watchlists/:id' do
      authenticate
+     binding.pry
      @watchlist = Watchlist.find_by(id: params[:id])
-     # if @stocks == []
-     #   redirect '/stocks/new'
-     # else
+     @stocks = @watchlist.stocks
      erb :'/watchlists/show'
-    # end
    end
 
    delete '/watchlists/:id' do
