6d3dd262b2c4d4b71ac007217af340d50c823455
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-06-08 12:12:29 -0400

    user has many stocks through watchlists added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base #figures
 
 has_secure_password
 has_many :watchlists
+has_many :stocks, through: :watchlist
 validates :username, uniqueness: true
 validates :username, presence: true
 validates :email, presence: true
