c7d5cf06eccc71839eaa5b3eb326f90eea80e7ea
Brittany <author.sincerelybr@gmail.com>
2020-06-08 12:07:16 -0400

    added the stock.new for params coming from API  and created a delete method for stocks

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,23 +1,14 @@
 class StockController < ApplicationController
 
-  # get '/stocks' do
-  #    authenticate
-  #    # @stock = current_user.stocks
-  #    @watchlists = current_user.watchlists
-  #    erb :'/stocks/index'
-  #  end
-
    get '/stocks/new' do
      authenticate
      erb :'/stocks/new'
    end
 
-  post '/stocks/new' do
-     authenticate
-     binding.pry
-     erb :'/stocks/new'
-   end
-
+  # post '/stocks/new' do
+  #    authenticate
+  #    erb :'/stocks/new'
+  #  end
 
    post '/stocks' do
      authenticate
@@ -48,28 +39,29 @@ class StockController < ApplicationController
    #   # binding.pry
    #   erb :'/stocks/edit'
    # end
-   #
-   # patch '/stocks/:id' do
-   #   @stock = Stock.find_by(id: params[:id])
-   #   authorize(@stock)
-   #   if @stock.update(name: params[:stock][:name])
-   #     redirect '/stocks'
-   #   else
-   #     redirect "/stocks/#{@stock.id}/edit"
-   #   end
-   # end
-   #
-   # get '/stocks/:id' do
-   #   authenticate
-   #   @stock = Stock.find_by(id: params[:id])
-   #   erb :'/stocks/show'
-   # end
-   #
-   # delete '/stocks/:id' do
-   #   @stock = Stock.find_by(id: params[:id])
-   #   authorize(@stock)
-   #   @stock.destroy
-   #   redirect '/stocks'
-   # end
+
+   patch '/stocks/:id' do
+     @stock = Stock.find_by(id: params[:id])
+     authorize(@stock)
+     if @stock.update(name: params[:stock][:name])
+       redirect '/stocks'
+     else
+       redirect "/stocks/#{@stock.id}/edit"
+     end
+   end
+
+   get '/stocks/:id' do
+     authenticate
+     @stock = Stock.find_by(id: params[:id])
+     # erb :'/stocks/show'
+   end
+
+   delete '/stocks/:id' do
+     @watchlist_id = Stock.find_by(id: params[:id]).watchlists.ids[0]
+     @stock = Stock.find_by(id: params[:id])
+     # authorize(@stock)
+     @stock.destroy
+     redirect "/watchlists/#{@watchlist_id}"
+   end
 
 end
