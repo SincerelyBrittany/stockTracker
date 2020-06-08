552476a113bf0f44529f56936475d1303634330b
Brittany <author.sincerelybr@gmail.com>
2020-06-08 12:11:47 -0400

    changed watchlist to watchlists --plural

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ActiveRecord::Base #titles
   belongs_to :user
   has_many :watchlist_stocks
-  has_many :watchlist, through: :watchlist_stocks
+  has_many :watchlists, through: :watchlist_stocks
 
   def slug
   name.downcase.gsub(" ","-")
