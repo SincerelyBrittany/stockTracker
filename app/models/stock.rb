f1fac094dec3dbe45890769ee21b7601882d73e3
SincerelyBrittany <BrittanyRussell1993@gmail.com>
2020-06-13 20:20:45 -0400

    removed slug method

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,5 @@
 class Stock < ActiveRecord::Base #titles
-  belongs_to :user
   has_many :watchlist_stocks
   has_many :watchlists, through: :watchlist_stocks
-
-  def slug
-  name.downcase.gsub(" ","-")
-  end
 
 end
