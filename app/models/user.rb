6fa428096900b6375eed15f0b044fe333dd95b46
Brittany <author.sincerelybr@gmail.com>
2020-06-13 20:21:24 -0400

    removed slug methods and pluralized watchlists

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,10 @@
 class User < ActiveRecord::Base #figures
 
 has_secure_password
-has_many :watchlists, dependent: :destroy
-has_many :stocks, through: :watchlist
+has_many :watchlists
+has_many :stocks, through: :watchlists
 validates :username, uniqueness: true
 validates :username, presence: true
 validates :email, presence: true
-
-def slug
-  self.username.downcase.tr(" ","-")
-end
-
-
-  def self.find_by_slug(slug)
-    User.all.find { |user| user.slug == slug }
-  end
 
 end
