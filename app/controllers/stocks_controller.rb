016d19aaceab717b6c5e82794756b7f1f91fb003
SincerelyBrittany <sincerelybr@gmail.com>
2020-06-16 12:33:50 -0400

    removed unnecessary methods

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -10,14 +10,8 @@ class StockController < ApplicationController
      authenticate
      @current_id = current_user.watchlists.find_by(id: params[:watchlist_id])
      searched_ticker = params[:stock][:name]
-     # begin
-       @info = APImanager.get_quote(searched_ticker)[0]
-     # rescue
-     #   raise NoStockError.new
-     # end
-     # if @info.nil?
-     #   redirect '/watchlists'
-     # end
+
+     @info = APImanager.get_quote(searched_ticker)[0]
      @stocks = Stock.new(name: @info[:name],
               ticker: @info[:ticker],
               price: @info[:price],
@@ -28,35 +22,11 @@ class StockController < ApplicationController
 
      erb :'/stocks/index'
    end
-   #
-   # get '/stocks/:id/edit' do
-   #   @stock = Stock.find_by(id: params[:id])
-   #   authorize(@stock)
-   #   # binding.pry
-   #   erb :'/stocks/edit'
-   # end
-
-   patch '/stocks/:id' do
-     @stock = Stock.find_by(id: params[:id])
-     authorize(@stock)
-     if @stock.update(name: params[:stock][:name])
-       redirect '/stocks'
-     else
-       redirect "/stocks/#{@stock.id}/edit"
-     end
-   end
-
-   get '/stocks/:id' do
-     authenticate
-     @stock = Stock.find_by(id: params[:id])
-     # erb :'/stocks/show'
-   end
 
    delete '/stocks/:id' do
      watchlist_id = Stock.find_by(id: params[:id]).watchlists.ids[0]
      @watchlist_id = watchlist_id.to_s
      @stock = Stock.find_by(id: params[:id])
-     # authorize(@stock)
      @stock.destroy
      redirect "/watchlists/#{@watchlist_id}"
    end
