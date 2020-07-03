71884ba1d10bf11d92ac99318941f74a475631c7
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-03 09:05:51 -0400

    prepares file for heroku

--- Rakefile
+++ Rakefile
@@ -2,6 +2,8 @@ ENV["SINATRA_ENV"] ||= "development"
 
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
+require 'sinatra/activerecord'
+
 
 desc "Start console"
 task :console do
--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,10 @@ end
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
 
 require_all 'app/errors'
 require_all 'app'
