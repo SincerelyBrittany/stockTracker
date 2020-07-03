1b72825621f2c2daae059ac81955c7f7cc8c820a
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-03 09:04:40 -0400

    adds migration from db:migrate

--- db/migrate/20200606210114_create_watchlists.rb
+++ db/migrate/20200606210114_create_watchlists.rb
@@ -1,4 +1,4 @@
-class CreateWatchlists < ActiveRecord::Migration
+class CreateWatchlists < ActiveRecord::Migration[4.2]
   def change
     create_table :watchlists do |t|
       t.string :name
--- db/migrate/20200606234431_create_watchlist_stocks.rb
+++ db/migrate/20200606234431_create_watchlist_stocks.rb
@@ -1,4 +1,4 @@
-class CreateWatchlistStocks < ActiveRecord::Migration
+class CreateWatchlistStocks < ActiveRecord::Migration[4.2]
   def change
     create_table :watchlist_stocks do |t|
       t.integer :watchlist_id
